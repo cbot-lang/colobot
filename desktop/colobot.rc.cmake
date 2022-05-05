463f9a70ed278f435abf489cd63224de9d28fe44
art-lotnik <93158916+art-lotnik@users.noreply.github.com>
2022-05-05 16:07:12 +0200

    Update colobot.rc.cmake

--- desktop/colobot.rc.cmake
+++ desktop/colobot.rc.cmake
@@ -12,8 +12,8 @@ BEGIN
 			VALUE "InternalName", "colobot\0"
 			VALUE "FileDescription", "Colobot: Gold Edition - Colonize with Bots\0"
 			VALUE "ProductName", "Colobot: Gold Edition\0"
-			VALUE "CompanyName", "TerranovaTeam\0"
-			VALUE "LegalCopyright", "Copyright (c) 2012-2014 TerranovaTeam\0"
+			VALUE "CompanyName", "TerranovaTeam & AZ\0"
+			VALUE "LegalCopyright", "Copyright (c) 2012-2022 TerranovaTeam\0"
 			VALUE "FileVersion", "@COLOBOT_VERSION_FULL@\0"
 			VALUE "ProductVersion", "@COLOBOT_VERSION_FULL@\0"
 			END
